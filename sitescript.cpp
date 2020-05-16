2b422ba29d0f9ad57a80628a20a9aad62dee064a
Kai Lyons <62919224+KaiLikesLinux@users.noreply.github.com>
2020-05-16 03:18:19 -0600

    Create sitescript.cpp

---
+++ sitescript.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int siteScriptCompile(void){
+    cout << "success!" << endl;
+}
+
+bool hasEnding (string const &fullString, string const &ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+int main( int argc, char *argv[] ){
+    if (argv[1] == NULL){
+        cout << "ERROR: No file arguments" << endl <<
+        "Usage: " << argv[0] << " programName" << endl;
+        return 1;
+    } else if(argc - 1 > 1){
+        cout << "ERROR: Too many arguments" << endl <<
+        "Usage: " << argv[0] << " programName" << endl;
+    } else {
+        if (!hasEnding(argv[1], ".ss")){
+            cout << "ERROR: Invalid file name!" << endl <<
+            "File Extension: .ss" << endl;
+        } else {
+            siteScriptCompile();   
+        }
+    }
+}
